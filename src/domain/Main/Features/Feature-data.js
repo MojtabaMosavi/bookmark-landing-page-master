efecaa831912a24f3650fd4f2d1de59a66c05bd0
Mojtaba Mosavi <Mosevi.m@yahoo.com>
2022-03-09 10:16:39 +0100

    Add features component
    Moved ralated images to component folder
    Modified margin-bottom of Hero

---
+++ src/domain/Main/Features/Feature-data.js
@@ -0,0 +1,30 @@
+import image1 from "./illustration-features-tab-1.svg";
+import image2 from "./illustration-features-tab-2.svg";
+import image3 from "./illustration-features-tab-3.svg";
+
+export default [
+    {
+        "searchName":"Simple Bookmarking",
+        "heading": "Bookmark in one click",
+        "description": "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.",
+        "cta":"More info",
+        image:image1,
+        id:0
+    },
+    {
+        "searchName":"Speedy Searching",
+        "heading":"Intelligent search",
+        "description": "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.",
+        "cta":"More info",
+        id:1,
+        image:image2
+    },
+    {
+        "searchName":"Easy Sharing",
+        "heading":"Share your bookmarks",
+        "description": "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button.",
+        "cta":"More info",
+        id:2,
+        image:image3
+    }
+]
---
+++ src/domain/Main/Features/Features.js
@@ -0,0 +1,20 @@
+import React from "react";
+import data from "./Feature-data";
+import "./features.scss";
+
+const Features = () => {
+    return (
+        <section className="features">
+            <div className="features__wrapper">
+                <div className="features__content">
+                    <h2 className="features__heading">Features</h2>
+                    <p className="features__text">Our aim is to make it quick and easy for you to access your 
+                    favourite websites. Your bookmarks sync between your devices so you can access them on the 
+                    go.</p>
+                </div>
+            </div>
+        </section>
+    );
+}
+ 
+export default Features;
