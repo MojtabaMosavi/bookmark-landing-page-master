26dfcd7a09c499aba6e9c8a3fbab3cc45c50d0a9
Mojtaba Mosavi <Mosevi.m@yahoo.com>
2022-03-22 07:31:11 +0100

    (feat): Add footer nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from "react";
 import Header from "./domain/Header/Header";
 import Main from "./domain/Main/Main";
+import Footer from "Domain//Footer/Footer";
 
 function App() {
     return (
         <Fragment>
             <Header/>
             <Main/>
+            <Footer/>
         </Fragment>
     );
 }
---
+++ src/domain/Footer/footer-data.js
@@ -0,0 +1,9 @@
+import facebookIcon from "Images/icon-facebook.svg";
+import twitterIcon from "Images/icon-twitter.svg";
+
+
+const links = ["Features","Pricing","Contact"]
+const icons = [{name:"facebook",icon:facebookIcon},{name:"twitter",icon:twitterIcon}]
+
+
+export {links,icons}
